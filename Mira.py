aadba9c31a93c14815fd927cba2311a1ff3028c8
yLIue <2138347243@qq.com>
2025-11-16 00:09:34 +0800

    '根据debug模块内容增加setLocalPath,增加了命令解析resolveArgs函数,初始化initMira增加了新的命令全组局变量，增加了匹配命令getCommand函数,给Config和Help预留了位置'

--- Mira.py
+++ Mira.py
@@ -1,21 +1,67 @@
 import os
-from debugtools import Debug
+from debugtools import *
+import sys
 
+# initGlobal
+PATH = '\\'.join(sys.argv[0].split('\\')[:-1]) + '\\'
 # debug
 debug = Debug('Mira', True)
 debug.logSet(_fillFunc=True)
 debug.showDebugLog()
+debug.setLocalPath(PATH)
 debug.cleanDebug()
 # global
-PATH = '\\'.join(__file__.split('\\')[:-1]) + '\\'
 LOCAL_PATH = debug.DebugPath(PATH)
+COMMANDS = {}
 
 
-def initMira():
+def getCommand(_command: str, _commands: dict) -> callable or None:
+    listMatcher = [_matcher for _matcher in _commands.keys() if _matcher.startswith(_command)]
+    if len(listMatcher) == 0:
+        debug.logError(f'命令解析失败:不存在 {_command} 命令')
+        print(f"无法识别的命令 '{_command}' ")
+        Help()
+        return None
+    if len(listMatcher) > 1:
+        debug.logError(f'命令解析失败:多个命令:{listMatcher}')
+        print('指令冲突 冲突指令:')
+        for i in listMatcher:
+            print(i)
+        return None
+    return _commands[listMatcher[0]]
+
+
+def Config():
+    print('Config')
+
+
+def Help():
+    print('Help')
+
+
+def resolveArgs(_args: list[str]):
+    if not len(_args):
+        debug.log('无指令')
+        print('没有任何参数')
+        Help()
+        return
+    try:
+        _command = getCommand(_args[0], COMMANDS)()
+    except TypeError:
+        return
+
+
+def initMira() -> None:
+    global COMMANDS
+    COMMANDS = {
+        'config': Config,
+        'c': Config,
+        'help': Help
+    }
     try:
         os.mkdir(LOCAL_PATH + 'config')
         with open(LOCAL_PATH + 'config\\user.cfg', 'w') as f:
-            f.write('userName:default\nuserEmail:default@mira.com')
+            f.write('user.name:default\nuser.email:default@mira.com')
         debug.logOk('初始化成功')
     except FileExistsError:
         debug.logError('初始化失败:配置文件存在')
@@ -24,9 +70,14 @@ def initMira():
 def readConfig() -> dict:
     with open(LOCAL_PATH + 'config\\user.cfg', 'r') as f:
         _infos = {}
-        for _item in f.readlines():
-            _itemInfo = _item.split('\n')[0].split(':')
-            _infos[_itemInfo[0]] = _itemInfo[1]
+        for _line in f.readlines():
+            _listInfo = _line.split('\n')[0].split(':')
+            _listTitle = _listInfo[0].split('.')
+            try:
+                _infos[_listTitle[0]][_listTitle[1]] = _listInfo[1]
+            except KeyError:
+                _dictTitle = {_listTitle[1]: _listInfo[1]}
+                _infos[_listTitle[0]] = _dictTitle
         debug.logOk(f'读取配置文件成功:{_infos}')
         return _infos
 
@@ -34,6 +85,8 @@ def readConfig() -> dict:
 def main() -> None:
     initMira()
     readConfig()
+    args = sys.argv[1:]
+    resolveArgs(args)
 
 
 if __name__ == '__main__':
