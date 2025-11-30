68acff559e6ab3dede2acf0c3045e8430f5b277f
yLIue <2138347243@qq.com>
2025-11-30 23:12:19 +0800

    Refactoring.将IniProcess优化了一下

---
+++ module/IniProcess.py
@@ -0,0 +1,40 @@
+from .Debug import debug
+
+
+def save(_path: str, _data: dict) -> None:
+    try:
+        with open(_path, 'a', encoding='utf-8') as f:
+            for title, dictKey in _data.items():
+                f.write(f'[{title}]\n')
+                for key, value in dictKey.items():
+                    f.write(f'\t{key} = {value}\n')
+                f.write('\n')
+        debug.log()
+        debug.logOk('保存配置文件成功')
+        debug.log(f'path: {_path}', 'OUTPUT')
+        debug.log(f'dict: {_data}', 'OUTPUT')
+        debug.log()
+    except FileNotFoundError:
+        debug.logError(f'保存配置文件失败 path: {_path}')
+        # raise FileNotFoundError
+
+
+def load(_path: str) -> dict:
+    _data = {}
+    title = None
+    try:
+        with open(_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                line = line.split('\n')[0]
+                if line.startswith('[') and line.endswith(']'):
+                    title = line[1:-1]
+                    _data[title] = {}
+                if '=' in line and title is not None:
+                    listKey = line.split('=')
+                    key = listKey[0].strip()
+                    value = listKey[1].strip()
+                    _data[title][key] = value
+        return _data
+    except FileNotFoundError:
+        debug.logError(f'读取配置文件失败 path: {_path}')
+        raise FileNotFoundError
