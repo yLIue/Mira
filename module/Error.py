da81637814e74c9cb59f55caa0980dbbda9ba091
yLIue <2138347243@qq.com>
2025-12-01 14:36:33 +0800

    Error.增加了NoFindCommand(命令解析失败)错误

--- module/Error.py
+++ module/Error.py
@@ -7,3 +7,13 @@ def NoArgs():
     debug.logError('没有任何指令', _func)
     debug.log()
     print('没有任何参数')
+
+
+def NoFindCommand(_command, _commands):
+    _func = debug.getFunc(1)
+    debug.log()
+    debug.logError(f'命令解析失败: 无法识别', _func)
+    debug.log(f'command: {_command}', 'OUTPUT', _func)
+    debug.log(f'commands: {_commands}', 'OUTPUT', _func)
+    debug.log()
+    print(f"无法识别的命令 '{_command}'")
