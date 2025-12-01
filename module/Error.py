0d136559a74bbb1f3e82d42484655434ef56d167
yLIue <2138347243@qq.com>
2025-12-01 11:10:13 +0800

    去除了Error模块的多余打印空格

--- module/Error.py
+++ module/Error.py
@@ -4,7 +4,6 @@ from .Debug import debug
 def NoArgs():
     _func = debug.getFunc(1)
     debug.log()
-    debug.logError('没有任何指令',_func)
+    debug.logError('没有任何指令', _func)
     debug.log()
-    print()
     print('没有任何参数')
