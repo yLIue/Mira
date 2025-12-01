f1e8575ef65fff3d127114317e314e1796b1dbab
yLIue <2138347243@qq.com>
2025-12-01 14:37:38 +0800

    getCommand.将里面的错误提示换为NoFindCommand

--- module/getCommand.py
+++ module/getCommand.py
@@ -1,15 +1,11 @@
 from module import debug
+from module import Error
 
 
 def getCommand(_command: str, _commands: list[str]) -> str | None:
     matcher = [_matcher for _matcher in _commands if _matcher.startswith(_command)]
     if len(matcher) == 0:
-        debug.log()
-        debug.logError(f'命令解析失败: 无法识别')
-        debug.log(f'command: {_command}', 'OUTPUT')
-        debug.log()
-
-        print(f"无法识别的命令 '{_command}'")
+        Error.NoFindCommand(_command, _commands)
         return None
     if len(matcher) > 1:
 
