7a4c6f69f58ca81dd74a7738a258db1a1daa23db
yLIue <2138347243@qq.com>
2025-12-19 18:33:07 +0800

    IniProcess.Refactoring-封装成为了一个类,IniFile文件

--- module/IniProcess.py
+++ module/IniProcess.py
@@ -1,45 +1,49 @@
 from .Debug import debug
 
 
-def save(_path: str, _data: dict) -> None:
-    try:
-        with open(_path, 'w', encoding='utf-8') as f:
-            for title, dictKey in _data.items():
-                f.write(f'[{title}]\n')
-                for key, value in dictKey.items():
-                    f.write(f'\t{key} = {value}\n')
-                f.write('\n')
-        debug.log()
-        debug.logOk('保存配置文件成功')
-        debug.log(f'path: {_path}', 'OUTPUT')
-        debug.log(f'dict: {_data}', 'OUTPUT')
-        debug.log()
-    except FileNotFoundError:
-        debug.logError(f'保存配置文件失败 path: {_path}')
-        # raise FileNotFoundError
+class IniFile:
+    def __init__(self, _path):
+        self.path = _path
 
+    def save(self, _data: dict) -> None:
+        try:
+            with open(self.path, 'w', encoding='utf-8') as f:
+                for title, dictKey in _data.items():
+                    f.write(f'[{title}]\n')
+                    for key, value in dictKey.items():
+                        f.write(f'\t{key} = {value}\n')
+                    f.write('\n')
+            debug.log()
+            debug.logOk('保存配置文件成功')
+            debug.log(f'path: {self.path}', 'OUTPUT')
+            debug.log(f'dict: {_data}', 'OUTPUT')
+            debug.log()
+        except FileNotFoundError:
+            debug.logError(f'保存配置文件失败 path: {self.path}')
+        except AttributeError:
+            debug.logError(f'访问对象不存在,请检查字典是否正确')
 
-def load(_path: str) -> dict:
-    _data = {}
-    title = None
-    try:
-        with open(_path, 'r', encoding='utf-8') as f:
-            for line in f:
-                line = line.split('\n')[0]
-                if line.startswith('[') and line.endswith(']'):
-                    title = line[1:-1]
-                    _data[title] = {}
-                if '=' in line and title is not None:
-                    listKey = line.split('=')
-                    key = listKey[0].strip()
-                    value = listKey[1].strip()
-                    if value.startswith('[') and line.endswith(']'):
-                        value = loadList(value[1:-1])
-                    _data[title][key] = value
-        return _data
-    except FileNotFoundError:
-        debug.logError(f'读取配置文件失败 path: {_path}')
-        raise FileNotFoundError
+    def load(self) -> dict:
+        _data = {}
+        title = None
+        try:
+            with open(self.path, 'r', encoding='utf-8') as f:
+                for line in f:
+                    line = line.split('\n')[0]
+                    if line.startswith('[') and line.endswith(']'):
+                        title = line[1:-1]
+                        _data[title] = {}
+                    if '=' in line and title is not None:
+                        listKey = line.split('=')
+                        key = listKey[0].strip()
+                        value = listKey[1].strip()
+                        if value.startswith('[') and line.endswith(']'):
+                            value = loadList(value[1:-1])
+                        _data[title][key] = value
+            return _data
+        except FileNotFoundError:
+            debug.logError(f'读取配置文件失败 path: {self.path}')
+            raise FileNotFoundError
 
 
 def loadList(_str: str) -> list:
