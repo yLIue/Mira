a0ab50fe1a5d47db463e05f48b3f6e372a8fe73c
yLIue <2138347243@qq.com>
2025-12-19 18:51:10 +0800

    miraPath.引用Path类

--- miraPath.py
+++ miraPath.py
@@ -1,19 +1,4 @@
-from module import debug
-
-
-class Path:
-    def __init__(self, _path: str) -> None:
-        self.path = _path
-
-    def __str__(self):
-        return self.path
-
-    def __truediv__(self, _path: str):
-        return Path(self.path + '\\' + _path)
-
-    def __fspath__(self) -> str:
-        return self.path
-
+from module import debug, Path
 
 miraPath_path = '\\'.join(__file__.split('\\')[:-1]) + '\\'
 debug.setLocalPath(miraPath_path)
