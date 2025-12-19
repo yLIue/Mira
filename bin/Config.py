36c04b7d11438514416d5fc19aa6f585b00d05ff
yLIue <2138347243@qq.com>
2025-12-19 18:41:52 +0800

    Config.Refactoring-重构了config命令

--- bin/Config.py
+++ bin/Config.py
@@ -1,23 +1,26 @@
-from module import debug, Error, getCommand, IniProcess
+from module import debug, Error, IniFile
+import miraPath
 
 
-def Config(_args: list[str], _data: dict) -> None:
+def Config(_args: list[str]) -> None:
     debug.log(f'调用config函数,args: {_args}')
-
-    _commands = _data['commands']
-    _config = _data['config']
-    _path = _data['directory']['etc']['configFile']
 
     if len(_args) == 0:
         Error.NoArgs()
+        return
+    if len(_args) < 3:
+        print('传参有错误')
     if len(_args) == 3:
-        _command = getCommand(_args[0], _commands['long'])
-        if _command is None:
-            Error.NoFindCommand(_args[0], _commands['long'])
-            return
-        if _command == '--global':
+        if _args[0] == '--global':
+            _configIni = IniFile(miraPath.configIni)
+            _dict = _configIni.load()
             [_title, _key] = _args[1].split('.')
-            _config[_title][_key] = _value = _args[2]
-
-            IniProcess.save(_path, _config)
-
+            try:
+                _dict[_title][_key]
+            except KeyError:
+                print('没查到此配置文件')
+                return
+            _dict[_title][_key] = _value = _args[2]
+            _configIni.save(_dict)
+        else:
+            Error.NoFindCommand(_args[0], ['--global'])
