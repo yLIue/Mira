f6e790f00d593ddb469396c132507276c8f0a770
yLIue <2138347243@qq.com>
2025-12-21 01:08:14 +0800

    Add.add-基本开发完成

---
+++ bin/Add.py
@@ -0,0 +1,39 @@
+import os
+import repoPath
+from module import debug, writeBlob, Staging
+
+
+def Add(_args: list[str]):
+    debug.log(f'调用Add函数,args: {_args}')
+    if len(_args) != 1:
+        return
+    _file = _args[0]
+    if _file == '.':
+        debug.log(f'add all')
+        for root, dirs, files in os.walk('.'):
+            if '.mira' in root or not files:
+                continue
+            for file in files:
+                _path = f'{root}\\{file}'
+                addStaging(_path[2:])
+                debug.logOk(f'add {file}')
+        return
+    addStaging(_file)
+
+
+def addStaging(_file):
+    _path = f'{repoPath.workRoot}\\{_file}'
+    _staging = Staging(repoPath.root)
+    try:
+        with open(_path, 'rb') as f:
+            byteData = f.read()
+            _hash = writeBlob(byteData)
+            _staging.add(_file, _hash)
+        debug.logOk(f'add {_file}')
+    except FileNotFoundError:
+        try:
+            _staging.delete(_file)
+            debug.logOk(f'del {_file}')
+        except KeyError:
+            print(f'文件 {_file} 不存在')
+            debug.logError(f'不存在的文件')
