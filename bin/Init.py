bee6e805294d2aa589e95501dcd461ad8e52542b
yLIue <2138347243@qq.com>
2025-12-21 01:06:57 +0800

    Init.Refactoring

--- bin/Init.py
+++ bin/Init.py
@@ -1,41 +1,34 @@
-import os, subprocess, shutil
-from module import debug, Error, getCommand, IniProcess, getPath
+import os
+import subprocess
+import shutil
+import repoPath
+import miraInfo
+from module import debug, IniFile
 
-# global
-REPO_PATH = f'{os.getcwd()}\\.mira'
 
-
-def Init(_args: list[str], _data: dict) -> None:
+def Init(_args: list[str]) -> None:
     debug.log(f'调用Init函数,args: {_args}')
-
-    # 销毁仓库
-    DestroyRepo()
-
-    initRepoDirectory()
-
-
-def initRepoDirectory() -> None:
     try:
-        os.makedirs(REPO_PATH)
-        subprocess.run(f'attrib +h "{REPO_PATH}"', shell=True, check=True)
+        os.mkdir(repoPath.root)
+        subprocess.run(f'attrib +h "{repoPath.root}"', shell=True, check=True)
+        os.mkdir(repoPath.objects)
+        os.mkdir(repoPath.logs)
+        IniFile(repoPath.config).save({
+            'info': {
+                'version': miraInfo.version
+            }
+        })
         debug.logOk('.mira文件初始化成功')
-        _path = getPath('repo')
-        for _item in _path.keys():
-            if not (_item.endswith('File') or _item.endswith('Table')):
-                os.makedirs(_path[_item]['path'])
+        print(f'已初始化空的mira仓库,路径: {repoPath.root / ""}')
     except FileExistsError:
         debug.log()
         debug.logError('.mira文件初始化失败,文件存在')
         debug.log()
 
 
-
-
-
-
 def DestroyRepo():
     try:
-        shutil.rmtree(REPO_PATH)
+        shutil.rmtree(repoPath.root)
         debug.logOk('成功销毁仓库')
     except FileNotFoundError:
         pass
