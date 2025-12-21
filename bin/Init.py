bc141842cbd1b164d061f09db7d90d12e11625c0
yLIue <2138347243@qq.com>
2025-12-21 12:07:04 +0800

    Init.增加了重新初始化功能和提示语句

--- bin/Init.py
+++ bin/Init.py
@@ -3,32 +3,34 @@ import subprocess
 import shutil
 import repoPath
 import miraInfo
+import miraPrint
 from module import debug, IniFile
 
 
-def Init(_args: list[str]) -> None:
+def Init(_args: list[str] = None) -> None:
     debug.log(f'调用Init函数,args: {_args}')
     try:
-        os.mkdir(repoPath.root)
-        subprocess.run(f'attrib +h "{repoPath.root}"', shell=True, check=True)
-        os.mkdir(repoPath.objects)
-        os.mkdir(repoPath.logs)
-        IniFile(repoPath.config).save({
-            'info': {
-                'version': miraInfo.version
-            }
-        })
-        debug.logOk('.mira文件初始化成功')
-        print(f'已初始化空的mira仓库,路径: {repoPath.root / ""}')
+        initRepo()
+        miraPrint.initRepo(repoPath.root / "")
     except FileExistsError:
-        debug.log()
-        debug.logError('.mira文件初始化失败,文件存在')
-        debug.log()
+        DestroyRepo()
+        miraPrint.reInitRepo(repoPath.root / "")
+        initRepo()
 
 
 def DestroyRepo():
-    try:
-        shutil.rmtree(repoPath.root)
-        debug.logOk('成功销毁仓库')
-    except FileNotFoundError:
-        pass
+    shutil.rmtree(repoPath.root)
+    debug.logOk('成功销毁仓库')
+
+
+def initRepo():
+    os.mkdir(repoPath.root)
+    subprocess.run(f'attrib +h "{repoPath.root}"', shell=True, check=True)
+    os.mkdir(repoPath.objects)
+    os.mkdir(repoPath.logs)
+    IniFile(repoPath.config).save({
+        'info': {
+            'version': miraInfo.version
+        }
+    })
+    debug.logOk('.mira文件初始化成功')
