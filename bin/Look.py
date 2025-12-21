ed71f456255c75ba9cea035be78ec43206818ee4
yLIue <2138347243@qq.com>
2025-12-21 14:10:30 +0800

    Look.增加了更好的提示信息

--- bin/Look.py
+++ bin/Look.py
@@ -1,4 +1,4 @@
-from module import debug, isMira
+from module import debug, isMira, Error
 import repoPath
 
 
@@ -6,24 +6,30 @@ def Look(_args: list[str]):
     debug.log(f'调用Look函数,args: {_args}')
     if not isMira():
         return
+    if len(_args) != 1:
+        Error.LookArgError()
+        return
     _hash = _args[0]
     result = {}
     i = 0
-    with open(repoPath.objects / _hash[:2] / _hash[2:], 'rb') as f:
-        data = f.read()
-        while i < len(data):
-            j = data.index(b'\0', i)
-            header = data[i:j].decode()
-            mode, name = header.split(' ', 1)
+    try:
+        with open(repoPath.objects / _hash[:2] / _hash[2:], 'rb') as f:
+            data = f.read()
+            while i < len(data):
+                j = data.index(b'\0', i)
+                header = data[i:j].decode()
+                mode, name = header.split(' ', 1)
 
-            sha_bytes = data[j + 1:j + 21]
-            sha_hex = sha_bytes.hex()
+                sha_bytes = data[j + 1:j + 21]
+                sha_hex = sha_bytes.hex()
 
-            if mode == 'dir':
-                typ = 'tree'
-            else:
-                typ = 'blob'
-            result[name] = (typ, sha_hex)
+                if mode == 'dir':
+                    typ = 'tree'
+                else:
+                    typ = 'blob'
+                result[name] = (typ, sha_hex)
 
-            i = j + 21
-        print(result)
+                i = j + 21
+            print(result)
+    except FileNotFoundError:
+        print(f'致命错误: 记录不存在')
